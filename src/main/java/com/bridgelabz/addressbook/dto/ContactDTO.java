00cfe4b0028b66970e238abd9756d26f684e4813
dpp17 <durga123q.com>
2023-04-18 09:05:22 +0530

    Added:: Use Lombok Library for Logging

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,13 +1,43 @@
 package com.bridgelabz.addressbook.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.ToString;
+
+@ToString
 public class ContactDTO {
+
+    @NotNull(message = "FirstName Cannot be empty")
+    @Pattern(regexp = "^[A-Z][A-Za-z]{2,}$", message = "Start FirstName With Capital Letter")
     public String firstName;
+
+    @NotNull(message = "LastName Cannot be empty")
+    @Pattern(regexp = "^[A-Z][A-Za-z]{2,}$", message = "Start LastName With Capital Letter")
     public String lastName;
+
+    @NotBlank(message = "Address Cannot be empty")
+    @Pattern(regexp = "^[^@#$%^&*]+$", message = "Special Character can't be used")
     public String address;
+
     public String city;
+
+    @NotBlank(message = "State Cannot be empty")
     public String state;
+
+    @NotNull(message = "Email ID Cannot be empty")
+    @Pattern(regexp = "^[a-z_0-9]{2,}@gmail.com$", message = "give email in format :: xxxxxxxx@gmail.com")
     public String email;
+
+    @NotNull(message = "Zip Code Cannot be empty")
+    @Pattern(regexp = "^[1-9][0-9]{5}$", message = "Zip Code must be of 6 digits")
     public int zip;
+
+    @NotNull(message = "PhoneNumber Cannot be empty")
+    @Pattern(regexp = "^[6789][0-9]{9}$", message = "PhoneNumber must be of 10 digits and in format of 9xxxxxxxxx or 8xxxxxxxxx or 7xxxxxxxxx or 6xxxxxxxxx only")
     public String phoneNumber;
+
+    @NotNull(message = "Aadhar Card Number Cannot be empty")
+    @Pattern(regexp = "^[1-9][0-9]{15}$", message = "Aadhar Card Number must be of 16 digits start with non-zero digit")
     public long aadharCardNumber;
 }
--- src/main/java/com/bridgelabz/addressbook/model/ContactData.java
+++ src/main/java/com/bridgelabz/addressbook/model/ContactData.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.addressbook.model;
 
 import com.bridgelabz.addressbook.dto.ContactDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class ContactData {
 
     private int contact_id;
@@ -25,102 +30,5 @@ public class ContactData {
         this.zip = contactDTO.zip;
         this.phoneNumber = contactDTO.phoneNumber;
         this.aadharCardNumber = contactDTO.aadharCardNumber;
-    }
-
-    public ContactData() {
-    }
-
-    public int getContact_id() {
-        return contact_id;
-    }
-
-    public void setContact_id(int contact_id) {
-        this.contact_id = contact_id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public long getAadharCardNumber() {
-        return aadharCardNumber;
-    }
-
-    public void setAadharCardNumber(long aadharCardNumber) {
-        this.aadharCardNumber = aadharCardNumber;
-    }
-
-    @Override
-    public String toString() {
-        return  "contact_id= " + contact_id +
-                "FirstName= " + firstName +
-                "LastName= " + lastName +
-                "Address= " + address +
-                "City= " + city +
-                "State= " + state +
-                "Email= '" + email +
-                "Zip= " + zip +
-                "Phone Number= " + phoneNumber +
-                "Aadhar Card Number= " + aadharCardNumber;
     }
 }
